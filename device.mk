11dc6f1e38e9cf6e545c931c0311b8824fb38b2e
HDzungx <hdzungx@gmail.com>
2024-10-14 00:29:26 +0700

    munch: Remove unwanted packages

--- device.mk
+++ device.mk
@@ -467,6 +467,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
